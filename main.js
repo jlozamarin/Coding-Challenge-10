376b8ebf95f21483e3b0eedea636fde42a217f21
jlozamarin <jenniferlozamarin@usf.edu>
2024-10-19 03:41:43 -0400

    Add event listener to update price on size selection

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 
 // Step 2: Add Event Listeners for Product Selection
-let productPrice = document.getElementById("productPrice");
-let colorOptions = document.getElementById("colorOptions");
-let addPurchaseButton = document.getElementById("addPurchaseButton");
+document.addEventListener("DOMContentLoaded", () => {
+    let productPrice = document.getElementById("productPrice");
+    let colorOptions = document.getElementById("colorOptions");
+    let addPurchaseButton = document.getElementById("addPurchaseButton");
 
-colorOptions.addEventListener("change", (event) => { //event listener for the color dropdown
-    let setPrice = event.target.options[event.target.selectedIndex].dataset.price; 
-    productPrice.textContent = `$${setPrice}`; 
-});
+    colorOptions.addEventListener("change", (event) => { //event listener for the color dropdown
+        let setPrice = event.target.options[event.target.selectedIndex].dataset.price; 
+        productPrice.textContent = `$${setPrice}`; 
+    });
 
-addPurchaseButton.addEventListener("click", () => { //event listener for the "Shop Now" button
-    let chosenColor = colorOptions.options[colorOptions.selectedIndex].dataset.color; // Get the selected color
-    console.log(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
-    alert(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
+    addPurchaseButton.addEventListener("click", () => { //event listener for the "Shop Now" button
+        let chosenColor = colorOptions.options[colorOptions.selectedIndex].dataset.color; // Get the selected color
+        console.log(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
+        alert(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
+    });
 });
