008d10bebf54b7fa86badcd437d223ff38b4e529
jlozamarin <jenniferlozamarin@usf.edu>
2024-10-19 03:26:05 -0400

    Add event listener to update price on size selection

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// Step 2: Add Event Listeners for Product Selection
+let productPrice = document.getElementById("productPrice");
+let colorOptions = document.getElementById("colorOptions");
+let addPurchaseButton = document.getElementById("addPurchaseButton");
+
+colorOptions.addEventListener("change", (event) => { //event listener for the color dropdown
+    let setPrice = event.target.value; 
+    productPrice.textContent = `$${setPrice}`; 
+});
+
+addPurchaseButton.addEventListener("click", () => { //event listener for the "Shop Now" button
+    let chosenColor = colorOptions.options[colorOptions.selectedIndex].dataset.color; // Get the selected color
+    console.log(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
+    alert(`You bought the ${chosenColor} Coach Tabby Shoulder Bag for ${productPrice.textContent}.`);
+});
